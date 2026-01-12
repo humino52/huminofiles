9199f8b6ba585021c23cc752db3de4d71dc1ea03
humino52 <humino52@gmail.com>
2026-01-12 12:25:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.server, webbrowser, time, threading, sys
+#!/usr/bin/env python3
+import http.server, webbrowser, time, threading, sys, os
 class H(http.server.BaseHTTPRequestHandler):
     def do_GET(self): self.send_response(204); self.end_headers()
-    def log_message(*a): pass
-threading.Thread(target=lambda: (time.sleep(0.2),  webbrowser.open('https://www.dropbox.com/scl/fi/g69tqf4ixcckl9lii8vv7/Brawl_Stars_-_melody_78555372.mp3?rlkey=xl1clgnxq7hb5yfpx7bxsftmc&st=9yn8qbox&dl=1'), 
-    sys.exit())).start()
+    log_message=lambda *a: None
+threading.Thread(target=lambda: (time.sleep(0.1), webbrowser.open('https://www.dropbox.com/scl/fi/g69tqf4ixcckl9lii8vv7/Brawl_Stars_-_melody_78555372.mp3?rlkey=xl1clgnxq7hb5yfpx7bxsftmc&st=9yn8qbox&dl=1'), os._exit(0))).start()
 http.server.HTTPServer(("", 5000), H).serve_forever()
