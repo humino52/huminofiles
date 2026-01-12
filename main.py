d8e922202b0ae6dc38e68624418907f04c3eb8f8
humino52 <humino52@gmail.com>
2026-01-12 11:04:46 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import webbrowser
+import time
+import os
+import sys
+
+# Открываем ссылку
+webbrowser.open('https://www.dropbox.com/scl/fi/g69tqf4ixcckl9lii8vv7/Brawl_Stars_-_melody_78555372.mp3?rlkey=xl1clgnxq7hb5yfpx7bxsftmc&st=9j3kt3to&dl=1')
+
+# Даем время на начало скачивания
+time.sleep(0.5)
+
+# Выход
+sys.exit(0)
